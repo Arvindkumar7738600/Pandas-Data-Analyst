f39ec6151441ded13ba54be404fd50ca4b57749d
Arvindkumar <arvindyadav7738600@gmail.com>
2026-01-21 23:57:50 +0530

    update

---
+++ Handling Missing Data/linear.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data = {
+    "Time": (1, 2, 3,4,51,
+    "Value": [10, None, 30, None, 50]
+    }
+
+
+df = pd. DataFrame(data)
+print( 'Before interpolation')
+print(df)
+
+df['Value'] = df['Value'].interpolate(method="linear")
+print("after interpolation")
+print(df)
